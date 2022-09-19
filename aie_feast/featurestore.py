c7468d1468a6d51a0791897f008b665b468b408a
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-19 10:25:16 +0800

    update

--- aie_feast/featurestore.py
+++ aie_feast/featurestore.py
@@ -1,8 +1,9 @@
 class FeatureStore:
     def __init__(self, project_folder=None, url=None, token=None, projectID=None):
         if project_folder:
-            file_path = project_folder + r"/config.json"
+            file_path = project_folder + r"/feature_store.yml"
         elif url and token and projectID:
             pass
         else:
-            raise ValueError("config file or meta server shoule be provided")
+            raise ValueError("one of config file or meta server project should be provided")
+        # TODO: init
