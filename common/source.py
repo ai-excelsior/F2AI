eea012247040e16df7a7a2208d171341499e17f3
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-20 14:07:22 +0800

    update SourceConfig structure

--- common/source.py
+++ common/source.py
@@ -1,2 +1,13 @@
+from dataclasses import dataclass, field
+from typing import Dict
+
+
+@dataclass
 class SourceConfig:
     """realize the data sources(not database) relation"""
+
+    name: str
+    type: str = field(default_factory=str)
+    file_format: int = field(default_factory=int)
+    file_path: str = field(default_factory=str)
+    request_features: Dict = field(default_factory=dict)
