70e7842af0eaef212f0ea9d1e41cb9a3e7de4112
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-20 14:04:53 +0800

    update ConnectConfig structure

--- common/connect.py
+++ common/connect.py
@@ -1,4 +1,14 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
 class ConnectConfig:
     """realize the connection config"""
 
-    pass
+    type: str
+    host: str = field(default_factory=str)
+    port: int = field(default_factory=int)
+    database: str = field(default_factory=str)
+    schema: str = field(default_factory=str)
+    user: str = field(default_factory=str)
+    passwd: str = field(default_factory=str)
