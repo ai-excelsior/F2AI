cb4c2ba437e0b17eda35e2cf87a707ce8c29392e
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-20 14:11:17 +0800

    update FeatureViews LabelViews structure

--- aie_feast/views.py
+++ aie_feast/views.py
@@ -1,13 +1,25 @@
 from typing import Dict, Any, List, Union
+from dataclasses import dataclass, field
 
 
+@dataclass
 class FeatureViews:
     """realize object using one .yml"""
 
-    pass
+    entity: List[str]
+    features: List[str]
+    batch_source: str
+    ttl: str = field(default_factory=str)
+    exogenous: bool = field(default_factory=bool)
+    request_source: str = field(default_factory=str)
 
 
+@dataclass
 class LabelViews:
     """realize object using one .yml"""
 
-    pass
+    entity: List[str]
+    labels: List[str]
+    batch_source: str
+    ttl: str = field(default_factory=str)
+    request_source: str = field(default_factory=str)
