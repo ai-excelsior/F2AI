25c0cf821348d8558c028c526f867397250ece82
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-21 10:40:12 +0800

    update service

--- common/utils.py
+++ common/utils.py
@@ -11,6 +11,30 @@ def schema_to_dict(schema):
 
 
 def service_to_dict(schema):
-    return
-    # item_dict={}
-    # for item in schema:
+    item_dict = {}
+    for item in schema:
+        split_item = item.split(":")
+        if len(split_item) == 2 or len(split_item) == 3:
+            if split_item[0] not in item_dict:
+                item_dict.update(
+                    {
+                        split_item[0]: [
+                            {
+                                (split_item[1] if split_item[1] != "*" else "__all__"): None
+                                if len(split_item) == 2
+                                else split_item[2]
+                            }
+                        ]
+                    }
+                )
+            else:
+                item_dict[split_item[0]].append(
+                    {
+                        (split_item[1] if split_item[1] != "*" else "__all__"): None
+                        if len(split_item) == 2
+                        else split_item[2]
+                    }
+                )
+        else:
+            raise ValueError("Please make sure colon not in name of table or features")
+    return item_dict
