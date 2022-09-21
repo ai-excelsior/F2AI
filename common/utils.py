3a62ada73f7bc5e8f85c742a42fb0a01ecb5c702
xu-yizhou-faust <yizhou.xu@unianalysis.com>
2022-09-21 11:55:45 +0800

    modify

--- common/utils.py
+++ common/utils.py
@@ -14,27 +14,19 @@ def service_to_dict(schema):
     item_dict = {}
     for item in schema:
         split_item = item.split(":")
-        if len(split_item) == 2 or len(split_item) == 3:
+        if len(split_item) < 2:
+            raise ValueError("Please indicate features in table:feature format")
+        elif len(split_item) > 3:
+            raise ValueError("Please make sure colon not in name of table or features")
+        else:
+            split_item[1] = split_item[1] if split_item[1] != "*" else "__all__"
             if split_item[0] not in item_dict:
                 item_dict.update(
-                    {
-                        split_item[0]: [
-                            {
-                                (split_item[1] if split_item[1] != "*" else "__all__"): None
-                                if len(split_item) == 2
-                                else split_item[2]
-                            }
-                        ]
-                    }
+                    {split_item[0]: [{split_item[1]: None if len(split_item) == 2 else split_item[2]}]}
                 )
             else:
                 item_dict[split_item[0]].append(
-                    {
-                        (split_item[1] if split_item[1] != "*" else "__all__"): None
-                        if len(split_item) == 2
-                        else split_item[2]
-                    }
+                    {split_item[1]: None if len(split_item) == 2 else split_item[2]}
                 )
-        else:
-            raise ValueError("Please make sure colon not in name of table or features")
+
     return item_dict
