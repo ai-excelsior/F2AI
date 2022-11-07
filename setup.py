c6329b6304e68a82077a6bfa6cd74e3dfaae0f00
Xudong Han <xudong.han@code.unianalysis.com>
2022-11-07 16:08:33 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+from pip._internal.network.session import PipSession
+
+# install_reqs = parse_requirements('requirements.txt', session=PipSession())
+
+setup(
+    name='f2ai',
+    version='0.0.1',
+    description='',
+    long_description='...',
+    url='https://github.com/ai-excelsior/F2AI',
+    long_description_content_type="text/x-rst",
+    author='',
+    license='MIT',
+    zip_safe=False,
+    packages=['aie_feast'],
+)
