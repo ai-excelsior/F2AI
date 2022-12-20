e850dbcc2051cd2898fe98d43695a471d7562c3e
ylzhao <yinling.zhao@unianalysis.com>
2022-12-20 10:42:27 +0800

    connect redis

--- f2ai/online_stores/online_redis_store.py
+++ f2ai/online_stores/online_redis_store.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 import uuid
 from typing import Optional
 
 import pandas as pd
 from redis import Redis
 
-from ..definitions.online_store import FeatureView, OnlineStore, OnlineStoreType, Period, Source
+from pydantic import PrivateAttr
+
+from f2ai.definitions import FeatureView, OnlineStore, OnlineStoreType, Period, Source, online_store
 
 
 class OnlineRedisStore(OnlineStore):
@@ -14,8 +17,20 @@ class OnlineRedisStore(OnlineStore):
     db: int = 0
     password: str = ""
 
-    def __init__(self, host: str, port: int, db: int, password: str):
-        self.client = Redis(host=host, port=port, db=db, password=password)
+    _cilent: Optional[Redis] = PrivateAttr(default=None)
+
+    @property
+    def client(self):
+
+        if self._cilent is None:
+            self._cilent = Redis(
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                db=self.db,
+            )
+
+        return self._cilent
 
     def write_batch(self, featrue_view: FeatureView, project_name: str, dt: pd.DataFrame) -> Source:
         set_key = uuid.uuid4().hex[:8]
@@ -31,10 +46,21 @@ class OnlineRedisStore(OnlineStore):
     ) -> pd.DataFrame:
 
         if ttl:
-            min_entity_timestamp = entity_df["event_timestamp"].min() - ttl.to_pandas_dateoffset()
+            min_entity_timestamp = pd.to_datetime(datetime.now(), utc=True) - ttl.to_pandas_dateoffset()
 
-        zset_key = self.client.hget(hkey)
-        data = self.client.zrange(zset_key, start=0, end=-1, withscores=True)
-        data = data[data["event_timestamp"] >= min_entity_timestamp]
+        zset_key = self.client.hget(hkey.split(":")[0], hkey.split(":")[1])
+        data = self.client.zrangebyscore(
+            zset_key, min=min_entity_timestamp, max=pd.to_datetime(datetime.now(), utc=True), withscores=True
+        )
+        result_data = pd.merge(entity_df, data, on=entity_df.columns, how="inner")
 
-        return data
+        return result_data
+
+    def set_up():
+        pass
+
+
+if __name__ == "__main__":
+    online = OnlineRedisStore(host="localhost", port=6379, db=0, password="")
+    # online.psy_conn
+
