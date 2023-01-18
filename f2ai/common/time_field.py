247397c2c299d05766f0bc04f62d79f6d5cde4c7
ylzhao <yinling.zhao@unianalysis.com>
2023-01-18 17:28:45 +0800

    add time_field

---
+++ f2ai/common/time_field.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class TimeField(str, Enum):
+    DEFAULT_EVENT_TIMESTAMP_FIELD = "event_timestamp"
+    ENTITY_EVENT_TIMESTAMP_FIELD = "_entity_event_timestamp_"
+    SOURCE_EVENT_TIMESTAMP_FIELD = "_source_event_timestamp_"
+    QUERY_COL = "query_timestamp"
+    TIME_COL = "event_timestamp"
+    MATERIALIZE_TIME = "materialize_time"
