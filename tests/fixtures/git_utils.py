51b23c5c1c48a4ab98f811ae61c614898b084334
ylzhao <yinling.zhao@unianalysis.com>
2023-01-19 15:33:29 +0800

    set subprocess para check=False

--- tests/fixtures/git_utils.py
+++ tests/fixtures/git_utils.py
@@ -16,12 +16,12 @@ def git_clone(cwd: str, repo: str, branch: str):
 
 
 def git_reset(cwd: str, branch: str, mode: str = "hard"):
-    return subprocess.run(["git", "reset", f"--{mode}", branch], cwd=cwd, check=True)
+    return subprocess.run(["git", "reset", f"--{mode}", branch], cwd=cwd, check=False)
 
 
 def git_clean(cwd: str):
-    return subprocess.run(["git", "clean", "-df"], cwd=cwd, check=True)
+    return subprocess.run(["git", "clean", "-df"], cwd=cwd, check=False)
 
 
 def git_pull(cwd: str, branch: str):
-    return subprocess.run(["git", "pull", "--rebase", "origin", branch], cwd=cwd, check=True)
+    return subprocess.run(["git", "pull", "--rebase", "origin", branch], cwd=cwd, check=False)
